0f1e833dced2a7d8d0e4e0cd1c68fbafd8357b34
Nick Velloff <nick.velloff@gmail.com>
2011-07-12 17:04:31 -0700

    admin account added

--- invite/inviteservice.class.php
+++ invite/inviteservice.class.php
@@ -55,7 +55,7 @@ class InviteService extends ReqBase
 			$mail->IsMail(); // telling the class to use native PHP mail()
 			
 			try {
-			  $mail->SetFrom('beta@unitedweego.com', 'Nick Velloff');
+			  $mail->SetFrom('beta@unitedweego.com', 'Weego Admin');
 			  $mail->AddAddress($receiverEmail);
 			  $mail->Subject = 'You have been invited to ' . $event->eventTitle;
 			  $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
