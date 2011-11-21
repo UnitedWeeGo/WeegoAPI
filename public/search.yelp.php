cb9d98a851035ffbb0e3b89b2bbea83df0d9fa96
Nick Velloff <nick@unitedweego.com>
2011-11-21 15:34:05 -0800

    another mime test

--- public/search.yelp.php
+++ public/search.yelp.php
@@ -1,6 +1,6 @@
 <?php
 
-header ("Content-Type:text/javascript"); 
+header ("Content-Type:application/json"); 
 require_once '../apiRequestHandlers/search.yelp.class.php';
 
 $ref = new SearchYelpClass();
